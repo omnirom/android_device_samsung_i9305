e131693f077dfe3e0b5259a28ff6889534ad2945
markcs <mcampbellsmith@gmail.com>
2013-11-17 22:20:50 +1100

    i9305: updates to build and add 4G option

--- omni_i9305.mk
+++ omni_i9305.mk
@@ -22,9 +22,19 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9305/full_i9305.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := i9305
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := omni_i9305
 PRODUCT_DEVICE := i9305
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9305
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m3xx TARGET_DEVICE=m3 BUILD_FINGERPRINT="samsung/m3xx/m3:4.1.2/JZO54K/I9305XXBMA6:user/release-keys" PRIVATE_BUILD_DESC="m3xx-user 4.1.2 JZO54K I9305XXBMA6 release-keys"
