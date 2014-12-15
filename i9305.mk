df172f1fd124e57cc4f871a5574fa0588ee534e8
DerTeufel <dominik-kassel@gmx.de>
2014-12-15 21:26:04 +0100

    revert ril blobs

--- i9305.mk
+++ i9305.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
-    ro.telephony.ril.v3=newDriverCall,newDialCode
+    ro.telephony.ril.config=exynos4RadioState
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
